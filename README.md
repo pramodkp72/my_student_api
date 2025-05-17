93dfa01c209c3abd5f57209b6a431a7ea07143ce
pramodkp72 <pramod.kp72@gmail.com>
2025-05-17 13:35:35 +0530

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## My Student Api
+
+API endpoints for student related data.
+
+#### License
+
+MIT
---
+++ my_student_api/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "My Student Api",
+			"type": "module",
+			"label": _("My Student Api")
+		}
+	]
